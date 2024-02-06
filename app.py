d8f896e961780e799d08ad099d907c53a1fd7446
Fyke Lleva <floterina@gmail.com>
2024-02-06 09:28:05 +0800

    update base and data

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template
-
+import data
 app = Flask(__name__)
 
-#banner data
-banner1 = "Unlock Your Coding Potential with Code Monty: Where Learning Meets Mastery for Every Filipino"
-banner2 = "Join Us! now for free python course."
-
-
-#footer data
-about_dev = "Fyke Lleva, a dedicated solo web developer, envisions a more inclusive digital landscape for Filipinos. The creative force behind Code Monty, the developer channels his passion for coding and design into building an accessible platform."
-about_codemonty = "Discover the power of Code Monty, your gateway to the exciting world of coding with Python. Tailored for Filipinos, our learning platform offers free, accessible, and engaging IT courses. Elevate your skills, embrace the digital future, and code your success with Code Monty - where learning meets innovation."
 
 @app.route ('/')
 def home():
-    return render_template('base.html', about_dev=about_dev, about_codemonty=about_codemonty, banner1=banner1, banner2=banner2)
+    return render_template('base.html', about_dev=data.about_dev, about_codemonty=data.about_codemonty, banner1=data.banner1, banner2=data.banner2, table_of_content=data.table_of_content)
 
---
+++ data.py
@@ -0,0 +1,11 @@
+#banner data
+banner1 = "Unlock Your Coding Potential with Code Monty: Where Learning Meets Mastery for Every Filipino"
+banner2 = "Join Us! now for free python course."
+
+
+#footer data
+about_dev = "Fyke Lleva, a dedicated solo web developer, envisions a more inclusive digital landscape for Filipinos. The creative force behind Code Monty, the developer channels his passion for coding and design into building an accessible platform."
+about_codemonty = "Discover the power of Code Monty, your gateway to the exciting world of coding with Python. Tailored for Filipinos, our learning platform offers free, accessible, and engaging IT courses. Elevate your skills, embrace the digital future, and code your success with Code Monty - where learning meets innovation."
+
+#table of content
+table_of_content = ["Print Function", "Variables", "Data Types", "Operators", "Functions", "Conditional Statements", "Looping","Library","Object Oriented Programming"]
